f42b224882050df239d19dc5d29cb6770bf53a30
jatin chouhan <jatin@jatins-MacBook-Air.local>
2024-04-06 13:11:47 +0530

    add a controller and router for home controller

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.listen("port", function (err) {
+ app.use('/',require('./routes'));
+
+app.listen(port, function (err) {
   if (err) {
     // console.log("Error:", err);
     // indtead of the above we can also do it like this and its called interpolation
     console.log(`Error in running the server: ${err}`);
   }
   console.log(`Server is running on port: ${port}`);
-});
+});         
+  
